be24319262e8ca268a44fb70839d3794ffd73133
LarsingDash <l.villevoye@student.avans.nl>
2022-12-09 13:22:17 +0100

    Added DialogBox

---
+++ DORA/app/src/main/java/nl/a3/dora/ui/component/DialogBox.kt
@@ -0,0 +1,69 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
+package nl.a3.dora.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DialogBox(
+    showDialog: MutableState<Boolean>,
+    titleText: String,
+    description: String,
+    buttons: Map<String, () -> Unit>
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp),
+            color = Color.LightGray,
+        ) {
+            Column {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    color = Color.Gray,
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(0.dp, 10.dp),
+                        text = titleText,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Text(
+                    modifier = Modifier
+                        .padding(0.dp, 10.dp),
+                    text = description,
+                    textAlign = TextAlign.Center,
+                )
+
+                Row {
+                    for (set in buttons) {
+                        Button(
+                            onClick = {
+                                showDialog.value = false
+                                set.value.invoke()
+                            },
+                            colors = ButtonDefaults.buttonColors(Color.Gray)
+                        ) {
+                            Text(text = set.key)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- DORA/app/src/main/java/nl/a3/dora/ui/screens/HomeScreen.kt
+++ DORA/app/src/main/java/nl/a3/dora/ui/screens/HomeScreen.kt
@@ -1,9 +1,34 @@
 package nl.a3.dora.ui.screens
 
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import nl.a3.dora.ui.component.DialogBox
 
 @Composable
-fun HomeScreen () {
-    Text(text = "Home")
+fun HomeScreen() {
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+
+    Button(onClick = {
+        showDialog.value = true
+    }) {
+        Text(text = "Dialog je moeder")
+    }
+
+    if (showDialog.value) {
+        DialogBox(
+            showDialog,
+            "TestTitle",
+            "Juuuuuuuuuuuuuuuuuuuuuuuuu kanker moeder",
+            mapOf(
+                "No" to { println("no") },
+                "Maybe" to { println("maybe") },
+                "Yes" to { println("yes") }
+            )
+        )
+    }
 }
