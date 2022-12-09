1a7807447c713eef236c98a57bb7b112a16b0eb9
LarsingDash <l.villevoye@student.avans.nl>
2022-12-09 13:39:04 +0100

    Added dialog in HelpScreen

--- DORA/app/src/main/java/nl/a3/dora/ui/screens/HelpScreen.kt
+++ DORA/app/src/main/java/nl/a3/dora/ui/screens/HelpScreen.kt
@@ -1,9 +1,32 @@
 package nl.a3.dora.ui.screens
 
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import nl.a3.dora.ui.component.DialogBox
 
 @Composable
-fun HelpScreen () {
-    Text(text = "Help")
+fun HelpScreen() {
+    val displayDisplay = remember {
+        mutableStateOf(false)
+    }
+
+    Button(onClick = {
+        displayDisplay.value = true
+    }) {
+        Text(text = "help")
+    }
+
+    if (displayDisplay.value) {
+        DialogBox(
+            showDialog = displayDisplay,
+            titleText = "title",
+            description = "description",
+            buttons = mapOf(
+                "close" to {},
+            )
+        )
+    }
 }
